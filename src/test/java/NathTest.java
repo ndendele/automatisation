d0688a26008d6a27d73ae5145e5df0b0728360ce
Nathalie <nathalie.dendele@gmail.com>
2018-02-27 15:00:31 +0100

    git add project file

---
+++ src/test/java/NathTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NathTest {
+
+    WebDriver driver;
+    String recherche;
+
+    @Before
+    public void init(){
+        driver= new ChromeDriver();
+    }
+
+    @Test
+    public void test(){
+        recherche=System.getProperty("recherche");
+        driver.get("http://www.google.com");
+        WebElement search= driver.findElement(By.name("q"));
+        search.sendKeys(recherche);
+        driver.findElement(By.name("btnK")).click();
+    }
+
+    @Test
+    public void test2(){
+        driver.get("http://www.google.com");
+        WebElement search= driver.findElement(By.name("q"));
+        search.sendKeys("fromage");
+        driver.findElement(By.name("btnK")).click();
+    }
+
+    @After
+    public void quit(){
+        driver.quit();
+    }
+}
